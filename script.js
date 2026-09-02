5c23e9b3074bdc0369172cda5ca7d76fba4f99c5
Sumolegen <dayasumo123@gmail.com>
2026-09-02 12:01:25 +0530

    Footer

---
+++ script.js
@@ -0,0 +1,9 @@
+fetch("Footer.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("footer").innerHTML = data;
+    })
+    .catch(error => {
+        console.error("Footer loading error:", error);
+    });
+    
