2c860e4b7cae78a5bdffad326b7b28dd5a5a8996
Srileka2203 <srileka747@gmail.com>
2026-09-02 13:01:21 +0530

    about

--- script.js
+++ script.js
@@ -6,4 +6,11 @@ fetch("Footer.html")
     .catch(error => {
         console.error("Footer loading error:", error);
     });
-    
+    fetch("about.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("about").innerHTML = data;
+    })
+    .catch(error => {
+        console.error("Footer loading error:", error);
+    });
